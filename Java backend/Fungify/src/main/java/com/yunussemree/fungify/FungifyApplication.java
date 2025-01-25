22abc733db25eeb7736d394eab1284ec98f47bc8
YunussEmree <senyigityunusemre@gmail.com>
2025-01-25 21:40:27 +0300

    Java backend service initial commit

---
+++ Java backend/Fungify/src/main/java/com/yunussemree/fungify/FungifyApplication.java
@@ -0,0 +1,13 @@
+package com.yunussemree.fungify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FungifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FungifyApplication.class, args);
+	}
+
+}
---
+++ Java backend/Fungify/src/test/java/com/yunussemree/fungify/FungifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunussemree.fungify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FungifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
